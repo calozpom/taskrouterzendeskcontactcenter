26a89f21b43a1c5420d9787ab4d56ec2dc877a93
Al Cook <acook@twilio.com>
2016-05-04 13:56:24 -0700

    update

--- index.js
+++ index.js
@@ -3,16 +3,39 @@ var req = require('request');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
 const actions = {
   say(sessionId, context, message, cb) {
     console.log(message);
     cb();
   },
   merge(sessionId, context, entities, message, cb) {
+    // Retrieve the location entity and store it into a context field
+    const loc = firstEntityValue(entities, 'location');
+    if (loc) {
+      context.loc = loc;
+    }
     cb(context);
   },
   error(sessionId, context, error) {
     console.log(error.message);
+  },
+  ['fetch-weather'](sessionId, context, cb) {
+    // Here should go the api call, e.g.:
+    // context.forecast = apiCall(context.loc)
+    context.forecast = 'sunny';
+    cb(context);
   },
 };
 const Logger = require('node-wit').Logger;
@@ -41,7 +64,7 @@ app.get('/initiatebot', function(request, response) {
   console.log(request.query);
   console.log(request.query['Body']);
   const context = {};
-client.message(request.query['Body'], context, cb(error, data) => {
+client.message(request.query['Body'], context, (error, data) => {
   if (error) {
     console.log('Oops! Got an error: ' + error);
   } else {
