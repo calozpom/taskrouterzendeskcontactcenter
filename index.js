3c90c58df87094285416abaa63cf60e4c981f2c6
Al Cook <acook@twilio.com>
2016-05-04 16:20:40 -0700

    update

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ client.converse(user, 'what\'s the weather?', {}, (error, data) => {
     console.log('Yay, got Wit.ai merge response: ');
     console.log(data);
     console.log("requesting follow up");
-    askFollowup();
+    askFollowUp();
  
 
   }
