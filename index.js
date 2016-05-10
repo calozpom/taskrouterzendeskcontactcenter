580cb9f9babecda48306071031c84aaae89e9eda
Al Cook <acook@twilio.com>
2016-05-10 16:15:34 -0700

    update

--- index.js
+++ index.js
@@ -171,7 +171,8 @@ app.post('/initiatebot', function(request, response) {
   var addOnsData = JSON.parse(request.body.AddOns);
   console.log("did json parsing work?");
   console.log(addOnsData);
-  var friendlyName_first = addOnsData['results']['whitepages_pro_caller_identity']['result']['results']['belongs_to']['names']['first_name'];
+  var friendlyName_first = addOnsData['results']['whitepages_pro_caller_identity']['result']['results']['belongs_to'];
+  //['names']['first_name'];
   console.log(friendlyName_first);
 
 
