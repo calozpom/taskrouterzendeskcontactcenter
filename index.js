dbda6f8c9692d109cf95275b433fa50959a450d8
Al Cook <acook@twilio.com>
2016-05-09 22:03:57 -0700

    update

--- index.js
+++ index.js
@@ -171,6 +171,12 @@ app.get('/initiatebot', function(request, response) {
         console.log(newTask);
          var newTask =client.workspace.tasks.create({workflowSid: "WW4d526c9041d73060ca46d4011cf34b33", attributes: '{"type":"support"}'});
          console.log(newTask);
+         req
+        .post('https://taskrouter.twilio.com/v1/Workspaces/'+workspaceSid+'/Tasks').auth(accountSid,authToken).form({WorkflowSid:"WW4d526c9041d73060ca46d4011cf34b33",Attributes:{}})
+      .on('response', function(response) {
+        console.log(response.statusCode); 
+        console.log(response.headers);
+        console.log(response);
 
       }
 
