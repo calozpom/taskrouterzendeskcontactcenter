e9daaf18c790be21929bd8054eef58e26c7adda8
Al Cook <acook@twilio.com>
2016-05-10 13:24:25 -0700

    update

--- index.js
+++ index.js
@@ -231,6 +231,35 @@ app.post('/botresponse', function(request, response) {
   response.send('');
 });
 
+app.post('/sendsms', function(request, response) {
+  console.log(request.body);
+  console.log(request.body.message_to);
+  console.log(request.body.message_from);
+  console.log(request.body.text);
+  smsclient.sendMessage({
+
+    to:request.body.message_to, // Any number Twilio can deliver to
+    from: request.body.message_from, // A number you bought from Twilio and can use for outbound communication
+    body: request.body.text // body of the SMS message
+
+}, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+    if (!err) { // "err" is an error received during the request, if any
+
+        // "responseData" is a JavaScript object containing data received from Twilio.
+        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+        console.log(responseData.from); // outputs "+14506667788"
+        console.log(responseData.body); // outputs "word to your mother."
+
+    }
+    else{
+      console.log("there was an error");
+    }
+});
+}
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
