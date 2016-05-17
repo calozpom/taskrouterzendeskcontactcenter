0044e582f418f203be674e066f74eb161d125301
Al Cook <acook@twilio.com>
2016-05-17 16:24:20 -0700

    update

--- index.js
+++ index.js
@@ -180,6 +180,7 @@ app.post('/initiatebot', function(request, response) {
   try {
   
     var addOnsData = JSON.parse(request.body.AddOns);
+    console.log(addOnsData);
     var friendlyName_first = addOnsData['results']['whitepages_pro_caller_identity']['result']['results'][0]['belongs_to'][0]['names'][0]['first_name'];
     var friendlyName_last = addOnsData['results']['whitepages_pro_caller_identity']['result']['results'][0]['belongs_to'][0]['names'][0]['last_name'];
   }
