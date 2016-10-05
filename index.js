9ab2ed0680964b6c4bd15e2f9f994bda153feeb6
al <al@twilio.com>
2016-10-05 15:00:07 -0700

    update

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.get('/token', function(request, response) {
   capability.allowActivityUpdates();
   capability.allowReservationUpdates();
   var token = capability.generate(86400);
-  response.send({'test'});
+  response.send('test');
 });
 
 app.get('/visualize', function(request, response) {
