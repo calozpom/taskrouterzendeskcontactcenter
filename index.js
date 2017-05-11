115b7563b87729a57261d917bfb1b8a6f2e53edf
al cook <al@twilio.com>
2017-05-10 23:48:58 -0700

    update

--- index.js
+++ index.js
@@ -60,16 +60,6 @@ var smsclient = new twilio.RestClient(accountSid, authToken);
 
 var app = express();
 
-function epicRandomString(b) {
-  for (var a = (Math.random() * eval("1e" + ~~(50 * Math.random() + 50))).toString(36).split(""), c = 3; c < a.length; c++) c == ~~(Math.random() * c) + 1 && a[c].match(/[a-z]/) && (a[c] = a[c].toUpperCase());
-  a = a.join("");
-  a = a.substr(~~(Math.random() * ~~(a.length / 3)), ~~(Math.random() * (a.length - ~~(a.length / 3 * 2) + 1)) + ~~(a.length / 3 * 2));
-  if (24 > b) return b ? a.substr(a, b) : a;
-  a = a.substr(a, b);
-  if (a.length == b) return a;
-  for (; a.length < b;) a += epicRandomString();
-  return a.substr(0, b)
-};
 
 function askFollowUp(user) {
   client.converse(user, null, (error, data) => {
@@ -590,10 +580,12 @@ app.post('/botresponse', function(request, response) {
     } else {
       console.log("there was an error");
     }
+    console.log("trying to update old message list")
     myFirebase.child(meyaUserID[2]).push({
       'from': 'MeyaBot',
       'message': request.body.text
     });
+    console.log("now trying to update new message list")
     myFirebase.child("messageList").child(meyaUserID[2]).push({
       'from': 'MeyaBot',
       'message': request.body.text
